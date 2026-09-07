29d969c1a782e3929cffb2a492d251765eba5bea
hekalrezky26 <hekalrezky123@gmail.com>
2026-09-07 09:13:01 +0700

    [P-2] setup routing resource dan route group admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Resource routes praktikum
+Route::resource('books', BookController::class);
+Route::resource('categories', CategoryController::class)->except(['show']);
+Route::resource('members', MemberController::class);
+Route::resource('loans', LoanController::class);
+Route::put('/loans/{id}/kembalikan', [LoanController::class, 'kembalikan'])
+    ->name('loans.kembalikan');
+
+// Route Grouping prefix /admin (Tugas Pertemuan 2)
+Route::prefix('admin')->group(function () {
+    Route::get('/info', function () {
+        return 'Panel Admin Perpustakaan';
+    })->name('admin.info');
 });
